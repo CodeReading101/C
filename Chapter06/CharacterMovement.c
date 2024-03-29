b679ab7c5df83d2afe85a5ba35ffcd9386dc41c5
CodeReading101 <159878159+CodeReading101@users.noreply.github.com>
2024-03-29 14:43:35 +0900

    Add files via upload

---
+++ Chapter06/CharacterMovement.c
@@ -0,0 +1,54 @@
+// C 프로그래밍 - https://codereading101.github.io/C/
+// 소스파일 - https://github.com/CodeReading101/C/blob/main/Chapter06/CharacterMovement.c
+
+#include <stdio.h> 					
+
+// 프로그램시작 
+int main() {
+	int row = 5, column = 9;
+	char direction='\0';
+
+	// 맵 출력 
+	printf("\033[44m                 \033[0m \n" );
+	printf("\033[44m \033[0m               \033[44m \033[0m \n" );
+	printf("\033[44m \033[0m               \033[44m \033[0m \n" );
+	printf("\033[44m \033[0m               \033[44m \033[0m \n" );
+	printf("\033[44m \033[0m               \033[44m \033[0m \n" );
+	printf("\033[44m \033[0m               \033[44m \033[0m \n" );
+	printf("\033[44m \033[0m               \033[44m \033[0m \n" );
+	printf("\033[44m \033[0m               \033[44m \033[0m \n" );
+	printf("\033[44m                 \033[0m \n" );
+	
+	// 입력 방향에 따라 계속 캐릭터 이동시키기 
+	while ( ( direction != 'q' ) && ( direction != 'Q' ) ) {
+		// 먼저 캐릭터의 기존 위치 지우고 새 위치로 업데이트 
+		printf("\033[%d;%df  ",row,column);
+		switch( direction ) {
+			// W는 위로 이동 
+		case 'w': case 'W': 
+			row = ( row <= 2 ) ? 2 : row-1;
+			break;
+			// A는 왼쪽으로 이동 
+		case 'a': case 'A': 
+			column = ( column <= 2 ) ? 2 : column-1;
+			break;
+			// S는 아래로 이동 
+		case 's': case 'S': 
+			row = ( row >= 8 ) ? 8 : row+1;
+			break;
+			// D는 오른쪽으로 이동 
+		case 'd': case 'D': 
+			column = ( column >= 15 ) ? 15 : column+1;
+			break;
+		}
+		printf("\033[%d;%df옷",row,column);
+
+		// 캐릭터의 새 이동 방향 입력 받기 
+		printf("\033[10;1fWASD 또는 Q와[Enter]를 입력하세요:         \033[10;38f" );
+		scanf("\n%c",&direction);
+	} 
+
+	// 프로그램 종료 
+	return 0;
+}
+
---
+++ Chapter07/RollingBall.c
@@ -0,0 +1,26 @@
+// C 프로그래밍 - https://codereading101.github.io/C/
+// 소스파일 - https://github.com/CodeReading101/C/blob/main/Chapter07/RollingBall.c
+
+#include <stdio.h>
+#include <unistd.h>  
+
+// 프로그램 시작 
+int main() {
+	int frameLength = 0, frame = 0, blank = 0;
+
+	// 애니메이션 총 프레임 길이 입력 받기 
+	printf( "애니메이션 총 프레임 길이를 입력하세요: ");
+	scanf( "%d", &frameLength );
+
+	// 첫번째 프레임부터 마지막 프레임까지 
+	for( frame = 1; frame <= frameLength; frame ++ ) {
+		// 1초마다 굴러가는 공 출력  
+		// sleep( 1 );				
+		printf ( "\033[2;%df O", frame );
+		// fflush( stdout );
+	}
+
+	//프로그램 종료  
+	return 0;
+}
+
