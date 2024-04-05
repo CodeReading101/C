886e477c673d710fb658213a48dfbc197a4a7386
CodeReading101 <159878159+CodeReading101@users.noreply.github.com>
2024-04-05 19:46:27 +0900

    Update ScoreSort.c

--- Chapter08/ScoreSort.c
+++ Chapter08/ScoreSort.c
@@ -5,13 +5,13 @@
 
 // 프로그램시작
 int main() {
-	int score[3] = { 0 };
+	int score[5] = { 0 };
 	int index = 0;
 	int last = 0;
 	int temp = 0;
 
 	// 점수들 입력
-	for( index = 0; index < 3; index++ )
+	for( index = 0; index < 5; index++ )
 	{
 		printf( "0점 ~ 100점 사이의 점수를 입력하세요: " );
 		scanf( "%d", &score[index] );
@@ -19,7 +19,7 @@ int main() {
 
 	// 점수 정렬
 	// 마지막 칸에 가장 큰 값을 저장하면서 한 칸씩 앞으로 영역 좁혀가기
-	for ( last = 2; last >= 0; last-- )
+	for ( last = 4; last >= 0; last-- )
 	{
 		// 영역의 첫번째 칸부터 인접한 두 값을 비교하면서
 		for ( index = 0; index < last; index++ )
@@ -35,7 +35,7 @@ int main() {
 	}
 
 	// 정렬된 점수 출력
-	for ( index = 0; index < 3; index++ )
+	for ( index = 0; index < 5; index++ )
 	{
 		printf( "%d) %d ", index + 1, score[index] );
 	}
