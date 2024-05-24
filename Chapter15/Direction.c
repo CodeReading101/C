d02922fc4fc974e63cf6e6308ecbc493508acada
CodeReading101 <159878159+CodeReading101@users.noreply.github.com>
2024-05-25 07:10:51 +0900

    Add files via upload

---
+++ Chapter15/Direction.c
@@ -0,0 +1,54 @@
+// C 프로그래밍 - https://codereading101.github.io/C/
+// 소스파일 - https://github.com/CodeReading101/C/blob/main/Chapter15/Direction.c
+
+#include <stdio.h>
+
+// 방향 매크로 상수 정의
+#define up 'w'
+#define UP 'W'
+#define down 's'
+#define DOWN 'S'
+#define left 'a'
+#define LEFT 'A'
+#define right 'd'
+#define RIGHT 'D'
+
+// 프로그램시작
+int main() {
+	char direction = UP;
+
+	// 캐릭터의 초기 위치를 출력
+	printf("      / \n");
+	printf(" [   옷   문\n");
+	printf("      -> \n\n");
+
+	// 이동 방향 입력 받기
+	printf("방향키(WASD)와[Enter]를 입력하세요: " );
+	scanf("%c",&direction);
+
+	// 이동 결과 출력
+	printf("\033[2;6f  ");
+	switch( direction ) {
+		// W는 위로 이동
+		case UP: case up:
+			printf("\033[1;6f 옷/ ");
+			break;
+		// A는 왼쪽으로 이동
+		case LEFT: case left:
+			printf("\033[2;1f 옷] ");
+			break;
+		// S는 아래로 이동
+		case DOWN: case down:
+			printf("\033[3;6f 옷-> ");
+			break;
+		// D는 오른쪽으로 이동
+		case RIGHT: case right:
+			printf("\033[1;1f              \n");
+			printf("   Success!!   \n");
+			printf("              ");
+	}
+
+	// 프로그램 종료
+	return 0;
+}
+
---
+++ Chapter15/RockPaperScissors.c
@@ -0,0 +1,74 @@
+// C 프로그래밍 - https://codereading101.github.io/C/
+// 소스파일 - https://github.com/CodeReading101/C/blob/main/Chapter15/RockPaperScissors.c
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// 가위바위보 매크로 상수 정의
+#define SCISSOR 1
+#define ROCK    2
+#define PAPER   3
+
+// A양이 승리한 경우 확인 매크로 함수
+#define DoesPlayerAWin( playerA, playerB )
+	// A양이 가위이고 B군이 보를 낸 경우 A양 승리
+	( ( ( playerA == SCISSOR ) && ( playerB == PAPER ) )
+	// A양이 바위이고 B군이 가위를 낸 경우 A양 승리
+	|| ( ( playerA == ROCK ) && ( playerB == SCISSOR ) )
+	// A양이 보이고 B군이 바위를 낸 경우 A양 승리
+	|| ( ( playerA == PAPER ) && ( playerB == ROCK ) ) )
+	? 1 : 0
+
+// 가위바위보 입력 함수
+int getRockPaperScissors() {
+	int player = 0;
+	// 사용자가 가위바위보중 하나를 선택
+	printf("가위(1), 바위(2), 보(3) 중 하나를 숫자로 입력하세요: ");
+	scanf( "%d", &player );
+
+	// 사용자가 선택한 결과를 출력
+	switch( player ) {
+	case SCISSOR:
+		printf( "가위를 냈습니다.\n" );
+		break;
+	case ROCK:
+		printf( "바위를 냈습니다.\n" );
+		break;
+	case PAPER:
+		printf( "보를 냈습니다.\n" );
+	}
+
+	return player;
+}
+
+// 프로그램시작
+int main() {
+	int playerA = SCISSOR;
+	int playerB = SCISSOR;
+
+	// A양이 가위바위보중 하나를 선택
+	printf("\nA양, ");
+	playerA = getRockPaperScissors();
+
+	// B군이 가위바위보중 하나를 선택
+	printf("\nB군, ");
+	playerB = getRockPaperScissors();
+
+	// A양과 B군의 가위바위보 승패 출력
+	// 먼저 A양과 B군이 비긴 경우 출력
+	if ( playerA == playerB ) {
+		printf( "\nA양과 B군이 비겼습니다.\n" );
+	}
+	// A양이 이긴 경우 출력
+	else if ( DoesPlayerAWin( playerA, playerB ) == 1) {
+		printf( "\nA양이 이겼습니다.\n" );
+	}
+	// B군이 이긴 경우 출력
+	else {
+		printf( "\nB군이 이겼습니다.\n" );
+	}
+
+	// 프로그램 종료
+	return 0;
+}
+
