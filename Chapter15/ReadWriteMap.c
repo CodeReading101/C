3a485cb9a3c502e4bd4f8d906cbc68cc4f200e8c
CodeReading101 <159878159+CodeReading101@users.noreply.github.com>
2024-05-27 01:53:18 +0900

    Update ReadWriteMap.c

--- Chapter15/ReadWriteMap.c
+++ Chapter15/ReadWriteMap.c
@@ -1,31 +1,25 @@
 // C 프로그래밍 - https://codereading101.github.io/C/
 // 소스파일 - https://github.com/CodeReading101/C/blob/main/Chapter15/ReadWriteMap.c
-
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 // 전역변수 맵
 extern char map[100][100];
 
 // 맵 읽기 함수
 int readMap( char *filename ) {
-	FILE *inFile = NULL;
-
 	// 맵 파일 열기
+	FILE *inFile = NULL;
 	if ( ( inFile = fopen( filename, "rt" ) ) == NULL ) {
 		printf( "[오류] 파일을 열 수 없습니다!" );
 		return -1;
 	}
 
 	// 맵 파일에서 한 줄씩 읽기
-	for( int row=0; fgets( map[row], 10, inFile ) != NULL; row++ ) {
+	for( int row=0; fgets( map[row], 100, inFile ) != NULL; row++ ) {
 		if ( map[row][0] == '\n' ) {
 			row--;
 		}
 	}
-
-//	for( int row=0; map[row][0]!= '\0'; row++ ) {
-//		printf("  (%d:%s)  .",row,map[row]);
-//	}
-//	printf("\n");
 
 	// 맵 파일 닫기
 	fclose( inFile );
@@ -39,7 +33,7 @@ void writeMap() {
 		for( int column = 0; map[row][column] != '\0'; column++ ) {
 			// 첫째 Wall이면 벽 출력
 			if( map[row][column] == 'W' )
-				printf("\033[44m \033[0m");
+				printf("\033[44m  \033[0m");
 			// 둘째 ' '이면 길 출력
 			else if( map[row][column] == ' ' )
 				printf("  ");
