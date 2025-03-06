e2b824fb5fb3b982d45cc735bd1966726a8de800
CodeReading101 <159878159+CodeReading101@users.noreply.github.com>
2025-03-06 16:43:26 +0900

    Add files via upload

--- Chapter02/BirthdaySong.c
+++ Chapter02/BirthdaySong.c
@@ -11,137 +11,137 @@ int main() {
 	// 생일 축하 노래 출력
 	// 생 출력
 	printf( "생" );
-	fflush( stdout );
+	// fflush( stdout );
 	// 1초 후
-	sleep(1);
+	// sleep(1);
 	// 일 출력
 	printf( "일 " );
-	fflush( stdout );
+	// fflush( stdout );
 	// 1초 후
-	sleep(1);
+	// sleep(1);
 	// 축 출력
 	printf( "축" );
-	fflush( stdout );
+	// fflush( stdout );
 	// 1초 후
-	sleep(1);
+	// sleep(1);
 	// 하 출력
 	printf( "하" );
-	fflush( stdout );
+	// fflush( stdout );
 	// 1초 후
-	sleep(1);
+	// sleep(1);
 	// 합 출력
 	printf( "합" );
-	fflush( stdout );
+	// fflush( stdout );
 	// 1초 후
-	sleep(1);
+	// sleep(1);
 	// 니 출력
 	printf( "니" );
-	fflush( stdout );
+	// fflush( stdout );
 	// 1초 후
-	sleep(1);
+	// sleep(1);
 	// 다 출력
 	printf( "다 " );
-	fflush( stdout );
+	// fflush( stdout );
 	// 1초 후
-	sleep(1);
+	// sleep(1);
 	// 생 출력
 	printf( "생" );
-	fflush( stdout );
+	// fflush( stdout );
 	// 1초 후
-	sleep(1);
+	// sleep(1);
 	// 일 출력
 	printf( "일 " );
-	fflush( stdout );
+	// fflush( stdout );
 	// 1초 후
-	sleep(1);
+	// sleep(1);
 	// 축 출력
 	printf( "축" );
-	fflush( stdout );
+	// fflush( stdout );
 	// 1초 후
-	sleep(1);
+	// sleep(1);
 	// 하 출력
 	printf( "하" );
-	fflush( stdout );
+	// fflush( stdout );
 	// 1초 후
-	sleep(1);
+	// sleep(1);
 	// 합 출력
 	printf( "합" );
-	fflush( stdout );
+	// fflush( stdout );
 	// 1초 후
-	sleep(1);
+	// sleep(1);
 	// 니 출력
 	printf( "니" );
-	fflush( stdout );
+	// fflush( stdout );
 	// 1초 후
-	sleep(1);
+	// sleep(1);
 	// 다 출력
 	printf( "다 " );
-	fflush( stdout );
+	// fflush( stdout );
 	// 1초 후
-	sleep(1);
+	// sleep(1);
 	// 사 출력
 	printf( "사" );
-	fflush( stdout );
+	// fflush( stdout );
 	// 1초 후
-	sleep(1);
+	// sleep(1);
 	// 랑 출력
 	printf( "랑" );
-	fflush( stdout );
+	// fflush( stdout );
 	// 1초 후
-	sleep(1);
+	// sleep(1);
 	// 하 출력
 	printf( "하" );
-	fflush( stdout );
+	// fflush( stdout );
 	// 1초 후
-	sleep(1);
+	// sleep(1);
 	// 는 출력
 	printf( "는 " );
-	fflush( stdout );
+	// fflush( stdout );
 	// 1초 후
-	sleep(1);
+	// sleep(1);
 	// 이름 출력
 	printf( "%s",name );
-	fflush( stdout );
+	// fflush( stdout );
 	// 1초 후
-	sleep(2);
+	// sleep(2);
 	// 의 출력
 	printf( "의 " );
-	fflush( stdout );
+	// fflush( stdout );
 	// 1초 후
-	sleep(1);
+	// sleep(1);
 	// 생 출력
 	printf( "생" );
-	fflush( stdout );
+	// fflush( stdout );
 	// 1초 후
-	sleep(1);
+	// sleep(1);
 	// 일 출력
 	printf( "일 " );
-	fflush( stdout );
+	// fflush( stdout );
 	// 1초 후
-	sleep(1);
+	// sleep(1);
 	// 축 출력
 	printf( "축" );
-	fflush( stdout );
+	// fflush( stdout );
 	// 1초 후
-	sleep(1);
+	// sleep(1);
 	// 하 출력
 	printf( "하" );
-	fflush( stdout );
+	// fflush( stdout );
 	// 1초 후
-	sleep(1);
+	// sleep(1);
 	// 합 출력
 	printf( "합" );
-	fflush( stdout );
+	// fflush( stdout );
 	// 1초 후
-	sleep(1);
+	// sleep(1);
 	// 니 출력
 	printf( "니" );
-	fflush( stdout );
+	// fflush( stdout );
 	// 1초 후
-	sleep(1);
+	// sleep(1);
 	// 다 출력
 	printf( "다" );
-	fflush( stdout );
+	// fflush( stdout );
 	return 0;
 }
 
