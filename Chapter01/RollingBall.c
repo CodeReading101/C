771712b96fdc0c4436a5252ca22ddb8736a4594f
CodeReading101 <159878159+CodeReading101@users.noreply.github.com>
2025-03-06 15:08:13 +0900

    Add files via upload

--- Chapter01/RollingBall.c
+++ Chapter01/RollingBall.c
@@ -6,23 +6,23 @@
 int main() {
 	// 첫번째 공 출력
 	printf( "\033[1;1f O" );
-	fflush( stdout );
+	// fflush( stdout );
 	// 1초 후 첫번째 공은 빈칸으로 덮어쓰고 옆 칸에 두번째 공 출력
-	sleep(1);
+	// sleep(1);
 	printf( "\033[1;2f O" );
-	fflush( stdout );
+	// fflush( stdout );
 	// 1초 후 두번째 공은 빈칸으로 덮어쓰고 옆 칸에 세번째 공 출력
-	sleep(1);
+	// sleep(1);
 	printf( "\033[1;3f O" );
-	fflush( stdout );
+	// fflush( stdout );
 	// 1초 후 세번째 공은 빈칸으로 덮어쓰고 옆 칸에 네번째 공 출력
-	sleep(1);
+	// sleep(1);
 	printf( "\033[1;4f O" );
-	fflush( stdout );
+	// fflush( stdout );
 	// 1초 후 네번째 공은 빈칸으로 덮어쓰고 옆 칸에 다섯번째 공 출력
-	sleep(1);
+	// sleep(1);
 	printf( "\033[1;5f O" );
-	fflush( stdout );
+	// fflush( stdout );
 	return 0;
 }
 
--- Chapter01/RollingDice.c
+++ Chapter01/RollingDice.c
@@ -11,46 +11,52 @@ int main() {
 	printf( "\033[3;1f |   O   | \n" );
 	printf( "\033[4;1f |       | \n" );
 	printf( "\033[5;1f +-------+ \n" );
+	// fflush( stdout );
 	// 1초 후 빈 화면에 2번 주사위 출력
-	sleep(1);
+	// sleep(1);
 	printf( "\033[2J" );
 	printf( "\033[1;51f +-------+ \n" );
 	printf( "\033[2;51f | O     | \n" );
 	printf( "\033[3;51f |       | \n" );
 	printf( "\033[4;51f |     O | \n" );
 	printf( "\033[5;51f +-------+ \n" );
+	// fflush( stdout );
 	// 1초 후 빈 화면에 3번 주사위 출력
-	sleep(1);
+	// sleep(1);
 	printf( "\033[2J" );
 	printf( "\033[1;31f +-------+ \n" );
 	printf( "\033[2;31f | O     | \n" );
 	printf( "\033[3;31f |   O   | \n" );
 	printf( "\033[4;31f |     O | \n" );
 	printf( "\033[5;31f +-------+ \n" );
+	// fflush( stdout );
 	// 1초 후 빈 화면에 4번 주사위 출력
-	sleep(1);
+	// sleep(1);
 	printf( "\033[2J" );
 	printf( "\033[1;11f +-------+ \n" );
 	printf( "\033[2;11f | O   O | \n" );
 	printf( "\033[3;11f |       | \n" );
 	printf( "\033[4;11f | O   O | \n" );
 	printf( "\033[5;11f +-------+ \n" );
+	// fflush( stdout );
 	// 1초 후 빈 화면에 5번 주사위 출력
-	sleep(1);
+	// sleep(1);
 	printf( "\033[2J" );
 	printf( "\033[1;21f +-------+ \n" );
 	printf( "\033[2;21f | O   O | \n" );
 	printf( "\033[3;21f |   O   | \n" );
 	printf( "\033[4;21f | O   O | \n" );
 	printf( "\033[5;21f +-------+ \n" );
+	// fflush( stdout );
 	// 1초 후 빈 화면에 6번 주사위 출력
-	sleep(1);
+	// sleep(1);
 	printf( "\033[2J" );
 	printf( "\033[1;41f +-------+ \n" );
 	printf( "\033[2;41f | O   O | \n" );
 	printf( "\033[3;41f | O   O | \n" );
 	printf( "\033[4;41f | O   O | \n" );
 	printf( "\033[5;41f +-------+ \n" );
+	// fflush( stdout );
 	return 0;
 }
 
